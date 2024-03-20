62378a575951352c33dc112403ab0988753f73b8
Sanagursky96 <usanagur96@gmail.com>
2024-03-20 04:03:59 +0200

    commit

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function getElementWidth (content, padding, border){
+  content = Number.parseFloat(content)
+  padding = Number.parseFloat(padding)
+  border = Number.parseFloat(border)
+
+  let total = content + ((padding + border) * 2);
+  return total
+  
+}
+console.log(getElementWidth("50px", "8px", "4px"))
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
+
+
